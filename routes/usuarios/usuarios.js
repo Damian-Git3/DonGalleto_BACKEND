5f3cb48a511936d7a0f400b8a293df98ddff2d63
tortadetamal <21000151@alumnos.utleon.edu.mx>
2024-03-04 15:29:23 -0600

    Pwned passwords

--- routes/usuarios/usuarios.js
+++ routes/usuarios/usuarios.js
@@ -1,32 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const logger = require('../../utils/logger');
-const UsuarioDao = require('../usuarios/usuarioDao');
-
+const UsuarioDao = require('../usuarios/usuarioDao')
+const pwnedpasswords = require('pwnedpasswords');
 
 router.post('/login', async function (req, res) {
 
   try {
-    logger.debug(req.body)
     let usuario = req.body;
+
     let result = await UsuarioDao.validarUsuario(usuario);
     if (result === undefined) {
-      res.status(400).send({ success: false, message: 'Usuario o Contraseña Incorrectos' });
+      res.status(200).send({ success: false, message: 'Usuario Autenticado' });
       return;
     }
 
-    res.status(200).send({success: true, message: 'Usuario Autenticado'});
+    res.status(200).send({ success: true, message: 'Usuario Autenticado' });
   } catch (error) {
     logger.error(`Error en login: ${error}`);
-    res.status(404).send({success: false, error: error});
+    res.status(404).send({ success: false, error: error });
   }
-
 });
 
-router.post('/registro', function (req, res) {
+router.post('/register', function (req, res) {
   try {
-    let result = UsuarioDao.registrarUsuario(req.body.usuario);
-    res.status(200).send({ succes: true, message: result });
+    pwnedpasswords(req.body.contrasena)
+      .then(count => {
+        if (count > 0) {
+          res.status(400).send({ success: false, message: 'Contraseña comprometida '+ count +' veces. Por favor, elige una contraseña más segura.'});
+        } else {
+          let result = UsuarioDao.saveUser(req.body);
+          res.status(200).send({ success: true, message: result });
+        }
+      })
   } catch (error) {
     res.status(404).send({ success: false, error: error });
   }
