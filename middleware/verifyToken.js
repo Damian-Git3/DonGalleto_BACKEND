f87ec883e0095c3ac38ad09d7c94bd2338dae3ca
IDGS-802-21000005 <CarlosIRBello@gmail.com>
2024-03-11 16:34:21 -0600

    roles y acciones de eliminar y modificar cuenta casi terminado

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 function verifyToken(req, res, next) {
-  const token = req.cookies['session'];
+  const token = req.headers["x-access-token"];
   if (!token) {
     return res
       .status(401)
@@ -9,7 +9,6 @@ function verifyToken(req, res, next) {
   }
 
   const decoded = jwt.verify(token, process.env.SECRET_KEY);
-  
   req.id = decoded.id
   next()
 }
